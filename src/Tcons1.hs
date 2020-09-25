df4e9e5580c54297965b9509eb1a07eda19c8c69
mlfbrown <mlfbrown@stanford.edu>
2020-09-25 10:34:29 -0700

    All tcons implementations except make

--- src/Tcons1.hs
+++ src/Tcons1.hs
@@ -24,6 +24,8 @@ import           Apron.Lincons1
 import           Apron.Scalar
 import           Apron.Tcons1
 import           Apron.Texpr1
+import           Control.Monad              (void)
+import           Control.Monad.State.Strict (liftIO)
 import           Data.Word
 
 tconsMake :: Constyp
@@ -34,10 +36,10 @@ tconsMake = undefined
 
 tconsFromLincons :: Lincons1
                  -> Abstract Tcons1
-tconsFromLincons = undefined
+tconsFromLincons = liftIO1 apTcons1FromLincons1Wrapper
 
 tconsCopy :: Tcons1 -> Abstract Tcons1
-tconsCopy = undefined
+tconsCopy = liftIO1 apTcons1CopyWrapper
 
 tconsPrint :: Tcons1 -> Abstract ()
 tconsPrint = liftIO1 apTcons1Print
@@ -45,39 +47,43 @@ tconsPrint = liftIO1 apTcons1Print
 -- Access
 
 tconsGetScalar :: Tcons1 -> Abstract Scalar
-tconsGetScalar = undefined
+tconsGetScalar = liftIO1 apTcons1ScalarrefWrapper
 
 tconsGetExpr :: Tcons1 -> Abstract Texpr1
-tconsGetExpr = undefined
+tconsGetExpr = liftIO1 apTcons1Texpr1refWrapper
 
 ---
 --- Tcons array
 ---
 
 tconsArrayMake :: Word32 -> Abstract Tcons1Array
-tconsArrayMake = undefined
+tconsArrayMake size = do
+  env <- getEnvironment
+  liftIO $ apTcons1ArrayMakeWrapper env $ fromIntegral size
 
 tconsArrayClear :: Tcons1Array -> Abstract ()
-tconsArrayClear = undefined
+tconsArrayClear = liftIO1 apTcons1ArrayClear
 
 -- Access
 
 tconsArraySize :: Tcons1Array -> Abstract Word32
-tconsArraySize = undefined
+tconsArraySize a = do
+  s <- liftIO $ apTcons1ArraySizeWrapper a -- Todo: checking?
+  return $ fromIntegral s
 
 tconsArrayGetIndex :: Tcons1Array -> Word32 -> Abstract Tcons1
-tconsArrayGetIndex = undefined
+tconsArrayGetIndex a i = liftIO $ apTcons1ArrayGetWrapper a $ fromIntegral i
 
 -- Setters
 
-tconsArrayClearIndex :: Tcons1Array -> Abstract ()
-tconsArrayClearIndex = undefined
+tconsArrayClearIndex :: Tcons1Array -> Word32 -> Abstract ()
+tconsArrayClearIndex a i = liftIO $ apTcons1ArrayClearIndexWrapper a $ fromIntegral i
 
-tconsArrayClearIndecies :: Tcons1Array -> Abstract ()
-tconsArrayClearIndecies = undefined
+tconsArrayClearIndecies :: Tcons1Array -> [Word32] -> Abstract ()
+tconsArrayClearIndecies a is = mapM_ (tconsArrayClearIndex a) is
 
 tconsArraySetIndex :: Tcons1Array -> Word32 -> Tcons1 -> Abstract ()
-tconsArraySetIndex = undefined
+tconsArraySetIndex a i v = void $ liftIO $ apTcons1ArraySet a (fromIntegral i) v
 
 tconsArraySetIndecies :: Tcons1Array -> [(Word32, Tcons1)] -> Abstract ()
-tconsArraySetIndecies = undefined
+tconsArraySetIndecies a vs = mapM_ (uncurry $ tconsArraySetIndex a) vs
