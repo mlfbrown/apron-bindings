b38703cd2d4f7c9141eeb2790b5e72800de55316
mlfbrown <mlfbrown@stanford.edu>
2020-09-26 14:15:59 -0700

    Add comment I accidentally stashed in examples

--- examples/Example2.hs
+++ examples/Example2.hs
@@ -7,16 +7,19 @@ main = evalAbstract defaultState $ do
   initAbstractState Intervals ["x", "y", "z"] ["u", "w", "v"]
   arr <- linconsArrayMake 3
 
+  --      -39x + 40y >= 0
   cons0 <- linconsEquation LINEXPR_SPARSE CONS_SUPEQ [ ("x", i (-39))
                                                      , ("y", i 40)
                                                      ] $ Just $ i 0
   linconsPrint cons0
 
+  --      -6x - 20y + 85 >= 0
   cons1 <- linconsEquation LINEXPR_SPARSE CONS_SUPEQ [ ("x", i (-6))
                                                      , ("y", i (-20))
                                                      ] $ Just $ i 85
   linconsPrint cons1
 
+  --      x >= 0
   cons2 <- linconsEquation LINEXPR_SPARSE CONS_SUPEQ [ ("x", i 1) ] $ Just $ i 0
   linconsPrint cons2
 
